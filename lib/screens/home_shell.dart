db1cfadbf2883b055e19a52d57e60b6ad9841934
Gavin200319 <gavinnyabera18@gmail.com>
2026-07-13 10:09:07 +0300

    update file

---
+++ lib/screens/home_shell.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'feed_screen.dart';
+import 'map_screen.dart';
+
+/// Root shell that holds the bottom nav bar switching between
+/// Feed (list of nearby drops) and Map (visual pin map).
+class HomeShell extends StatefulWidget {
+  const HomeShell({super.key});
+
+  @override
+  State<HomeShell> createState() => _HomeShellState();
+}
+
+class _HomeShellState extends State<HomeShell> {
+  int _currentIndex = 0;
+
+  final _screens = const [
+    FeedScreen(),
+    MapScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _screens,
+      ),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _currentIndex,
+        onDestinationSelected: (i) => setState(() => _currentIndex = i),
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.explore_outlined),
+            selectedIcon: Icon(Icons.explore),
+            label: 'Explore',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.map_outlined),
+            selectedIcon: Icon(Icons.map),
+            label: 'Map',
+          ),
+        ],
+      ),
+    );
+  }
+}
