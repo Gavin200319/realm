b64bc87b334b0ff20f3664dd3c88b9a613cce38e
Gavin200319 <gavinnyabera18@gmail.com>
2026-07-15 12:24:30 +0300

    Hide Size import from mapbox_maps_flutter

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:geolocator/geolocator.dart' as geo;
 import 'package:http/http.dart' as http;
-import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart' hide Size;
 import '../models/drop.dart';
 import '../services/location_service.dart';
 import '../services/onboarding_service.dart';
